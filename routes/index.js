33344c4288869aa5e9e25be6f44040f283aac6d5
Shintaro Kaneko <kaneshin0120@gmail.com>
2012-04-19 17:10:21 -0700

    Create search, register and delete pages.

--- routes/index.js
+++ routes/index.js
@@ -6,3 +6,16 @@
 exports.index = function(req, res){
   res.render('index', { title: 'Express' })
 };
+
+exports.search = function(req, res){
+  res.render('search', { title: 'Express' })
+};
+
+exports.register = function(req, res){
+  res.render('register', { title: 'Express' })
+};
+
+exports.delete = function(req, res){
+  res.render('delete', { title: 'Express' })
+};
+
